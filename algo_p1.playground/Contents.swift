173cfb9b202e2e3426c60fe0dce46c6ed221360b
mdeaconu <mihai.deaconu@icloud.com>
2018-08-24 00:14:40 +0300

    Add lsb routine

--- algo_p1.playground/Contents.swift
+++ algo_p1.playground/Contents.swift
@@ -16,6 +16,22 @@ fileprivate struct TwinBits {
         return core(n: number)
     }
     
+    var nextSmaller: UInt64 {
+        if number == 0 {
+            return 0
+        }
+        if number & (number - 1) == 0 {
+            return number >> 1
+        }
+        return 0
+    }
+    
+    private func findLSB(for n: UInt64) -> UInt64 {
+        let less = n - 1
+        let nn = (less | n) ^ less
+        return UInt64(log2(Double(nn + 1)))
+    }
+    
     private func core(n: UInt64) -> UInt64 {
         var copy_n = number
         var position: UInt8 = 0
